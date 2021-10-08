1abb0013884e12773629c30e6a8be4034c01508f
Brayam Henrik Vieira <brayam.henrik@algosoft.com.br>
2021-10-08 14:02:32 -0300

    Clear #1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      HELLO WORLD!
+      <br /><br />
+      <small>[ <a href="https://instagram.com/brayam.henrik" target="_blank" rel="noreferrer">by @Brayam.Henrik</a> ]</small>
+    </div>
+  );
+}
+
+export default App;
