ab6e9a4d7bc86dfd5c807bb69a0f45d15ef34bb4
Brayam <brayam.100@gmail.com>
2021-10-09 15:34:14 -0300

    Stable #4.0

--- src/components/core/Seo/index.js
+++ src/components/core/Seo/index.js
@@ -3,26 +3,26 @@ import { Helmet } from 'react-helmet-async';
 
 const HOST = window.location.protocol.concat("//").concat(window.location.host);
 
-function SEO ({ metaRobots, title, metaDescription, socialTitle, socialDescription, socialImage }) {
+function SEO (props) {
   return (
     <Helmet>
-      <meta name="robots" content={`${metaRobots ? metaRobots : "index, follow"}`} />
+      <meta name="robots" content={`${props.metaRobots ? props.metaRobots : "index, follow"}`} />
 
-      <title>{title}</title>
-      <meta name="description" content={metaDescription} />
+      <title>{props.title}</title>
+      <meta name="description" content={props.metaDescription} />
       <link rel="canonical" href={HOST} />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content={HOST} />
-      <meta property="og:title" content={socialTitle} />
-      <meta property="og:description" content={socialDescription} />
-      <meta property="og:image" content={HOST + socialImage} />
+      <meta property="og:title" content={props.socialTitle} />
+      <meta property="og:description" content={props.socialDescription} />
+      <meta property="og:image" content={HOST + props.socialImage} />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={HOST} />
-      <meta property="twitter:title" content={socialTitle} />
-      <meta property="twitter:description" content={socialDescription} />
-      <meta property="twitter:image" content={HOST + socialImage} />
+      <meta property="twitter:title" content={props.socialTitle} />
+      <meta property="twitter:description" content={props.socialDescription} />
+      <meta property="twitter:image" content={HOST + props.socialImage} />
     </Helmet>
   );
 };
